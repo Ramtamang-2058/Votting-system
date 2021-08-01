5fad94cd25cf5d59dcf99b43d7947eadecf774da
Sahaj Raj Malla <mallasahajraj@gmail.com>
2021-08-01 10:49:31 +0545

    sync with merged backend version without frontend

--- superuser/settings.py
+++ superuser/settings.py
@@ -67,9 +67,7 @@ ROOT_URLCONF = 'superuser.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR,'motfrontend/build'),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -162,13 +160,16 @@ MEDIA_URL = '/media/'
 # MEDIA_URL = 'media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-            # os.path.join(BASE_DIR,'media'),
-            os.path.join(BASE_DIR,'motfrontend/build/static'),
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS=[
+#             # os.path.join(BASE_DIR,'media'),
+#             os.path.join(BASE_DIR,'motfrontend/build/static'),
 
-]
+# ]
+
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- superuser/urls.py
+++ superuser/urls.py
@@ -17,24 +17,16 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.generic import TemplateView
-
+from django.conf.urls.static import static
 
 admin.site.site_header = 'Robotics Association of Nepal'
 
-urlpatterns = []
-
-from django.conf.urls.static import static
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [
+urlpatterns = [
     path('jet/', include('jet.urls', 'jet')), 
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
     path('about/', include('about.urls')),
-    path('',TemplateView.as_view(template_name='index.html')),
-    re_path('.*',TemplateView.as_view(template_name='index.html')),
-
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
